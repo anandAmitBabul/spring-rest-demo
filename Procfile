337c9fe387c7932beabb0130dde779fb055a81ed
Amit Anand <amit.baabul.anand@gmail.com>
2018-10-24 09:13:29 +0530

    updating pom file and adding extra functions
    updating pom file and adding extra functions

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:    java $JAVA_OPTS -jar target/dependency/spring-rest-demo.jar --port $PORT target/*.war
+web: java $JAVA_OPTS -cp target/classes:target/dependency/webapp-runner.jar --port $PORT target/*.war
--- src/main/java/io/javamit/springdemo/rest/StudentRestController.java
+++ src/main/java/io/javamit/springdemo/rest/StudentRestController.java
@@ -3,7 +3,13 @@ package io.javamit.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +19,32 @@ import io.javamit.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
-	@GetMapping("/students")
-	public List<Student> students()
+	private List<Student> theStudents;
+	
+	@PostConstruct
+	public void loadData()
 	{
-		List<Student> theStudents = new ArrayList<>();
-		
+		theStudents = new ArrayList<>();
 		theStudents.add(new Student("Poornima","K"));
 		theStudents.add(new Student("John","Doe"));
 		theStudents.add(new Student("Mario","Rossi"));
+	}
+	
+	@GetMapping("/students")
+	public List<Student> students()
+	{
+		
 		return theStudents;
 	}
+	
+	@GetMapping("/students/{studentid}")
+	public Student oneStudent(@PathVariable int studentid)
+	{	
+		if(studentid>=theStudents.size() || studentid <0)
+		{
+			throw new StudentNotFoundException("Student Id is not found - "+studentid);
+		}
+		return theStudents.get(studentid);
+	}
+	
 }
